557bee44d6d65caba3bd38a67c68f33a2c3da758
Tayyab Shahzad <Tayyab@DESKTOP-MQ8O506>
2022-06-19 22:28:53 +0500

    Add project files.

---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext
+                (serviceProvider.GetRequiredService
+                <DbContextOptions<LibraryContext>>()))
+            {
+                if(context.Book.Any())
+                {
+                    return;
+                }
+                context.AddRange(
+                    new Book { Title = "OOP", CallNumber = "10" },
+                    new Book { Title = "OS", CallNumber = "20" }
+                    );
+               context.SaveChanges();
+            }
+     
+        }
+    }
+}
